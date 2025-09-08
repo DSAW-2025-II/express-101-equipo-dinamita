7172d709c219e7ccdcade1c3e6c21ee06b02d96f
Carlos Vargas <carlosa.vargase@gmail.com>
2025-09-08 15:47:12 -0500

    add karol's info

--- routes/user-info.js
+++ routes/user-info.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/1', (req, res) => {
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
     res.json({
-        "name": process.env.NAME_1,
-        "lastName": process.env.LASTNAME_1,
-        "email": process.env.EMAIL_1,
-        "id": process.env.ID_1
+        "name": process.env[`NAME_${id}`],
+        "lastName": process.env[`LASTNAME_${id}`],
+        "email": process.env[`EMAIL_${id}`],
+        "id": process.env[`ID_${id}`]
     });
 });
 
